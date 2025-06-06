8b4eb3128aae9c78d9cb312ae8843fb75369cc71
Vitor Barros <mkg.vitorpaz@gmail.com>
2025-06-06 19:12:12 -0300

    starting configuration of docker and backendd

---
+++ backend/orderservice/src/test/java/com/gbrl/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbrl.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
